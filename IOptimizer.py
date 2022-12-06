30c03452074d37800c0a302c631673672b3a3ce3
13David00 <119938472+13David00@users.noreply.github.com>
2022-12-06 14:05:57 +0100

    Create IOptimizer.py
    Init Optimizer Interface

---
+++ IOptimizer.py
@@ -0,0 +1,11 @@
+import importlib, inspect
+class IOptimizer:
+    def __init__(self, dimension, lower_bound, upper_bound, eval_function, optimizer_name):
+        optimizer_module=importlib.import_module(optimizer_name)    #import optimizer module
+        for name, obj in inspect.getmembers(optimizer_module):            #search for class in optimizer module
+            if inspect.isclass(obj):
+                cl = obj                                            #get class
+        self.optimizer = cl(dimension, lower_bound, upper_bound, eval_function)  #init object of class
+    
+    def Optimize(self):
+        return self.optimizer.Optimize()
