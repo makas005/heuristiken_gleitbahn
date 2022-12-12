91c251923a6d11b4b13cb0fc2b2d477026c7b7a9
Max Kaspari <kasparimax@gmail.com>
2022-12-12 15:24:32 +0100

    segment: added error handling
    Added handling of negative numbers as root
    argument in Segment.py

--- Segment.py
+++ Segment.py
@@ -24,12 +24,21 @@ class Segment:
         s = math.sqrt(self.__delta_h**2+self.__length**2)
         a = self.__g*(self.__delta_h/s)
         if a==0:
+            #No acceleration
             if self.__v_initial==0:
+                #If no acceleration and no initial speed, then fail
                 t = -1
             else:
+                #Movement without acceleration
                 t = s/self.__v_initial   
         else:
-            t = (-self.__v_initial+math.sqrt(self.__v_initial**2+2*a*s))/(a)
+            root_argument = self.__v_initial**2+2*a*s
+            if root_argument < 0:
+                #Error case: negative root argument means acceleration is not great enough to climb to top of segment
+                t = -1
+            else:
+                #Formula to compute accelerated movement
+                t = (-self.__v_initial+math.sqrt(root_argument))/(a)
         return t
     
     def GetDeltaH(self):
